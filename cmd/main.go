f7bdb1aacf6f1f67fe8678689b63d4364cb4858c
cristianrb <cristianrb235@gmail.com>
2023-09-02 11:32:42 +0200

    implement CreateUser and LoginUser with gRPC

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"log"
+	"net"
+
 	"github.com/cristianrb/simplebank/api"
 	db "github.com/cristianrb/simplebank/db/sqlc"
+	"github.com/cristianrb/simplebank/gapi"
+	"github.com/cristianrb/simplebank/pb"
 	"github.com/cristianrb/simplebank/util"
-	"log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	_ "github.com/golang/mock/mockgen/model"
 	_ "github.com/lib/pq"
@@ -22,11 +28,38 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
